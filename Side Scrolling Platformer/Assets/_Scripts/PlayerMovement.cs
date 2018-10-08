da909f66029b3194c6304d9c7b73eae07970e51b
sarahc123 <g00330821@gmit.ie>
2018-10-08 17:34:03 +0100

    added sprites and made camera move with player

---
+++ Side Scrolling Platformer/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    public int playerSpeed = 10;
+    public int playerJump = 1250;
+    private float moveX = 0;
+
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //frames per second - 
+        playerMove();
+
+        //use mathf.clamp 
+        //float newX = mathf.clamp(rb.position.x, -1.95f , +1.95f); change -1.95 and 1.95 to variables get rid of magic numbers
+        //have newx value , need to reposition ridig body
+        //rb.position = new vector2(newX,rb,potition.y);
+
+    }
+    void playerMove()
+    {
+        //controls
+        moveX = Input.GetAxis("Horizontal");
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();
+        }
+        //animation
+        // Physics
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);// allows the player to move left and right
+    }
+    void Jump()
+    {
+        //jumping
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJump);
+
+
+    }
+}
---
+++ Side Scrolling Platformer/Assets/cameraSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSystem : MonoBehaviour {
+
+    public GameObject player;
+    //clamp the camera to a certain position
+    public float xmin;
+    public float xmax;
+    public float ymin;
+    public float ymax;
+
+    // Use this for initialization
+    void Start () {
+        //TAG the player in unity as Player which can then be accessed from any script
+        player= GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// LateUpdate gets called at the end of the update
+	void LateUpdate () {
+        float x = Mathf.Clamp(player.transform.position.x, xmin, xmax);
+        float y = Mathf.Clamp(player.transform.position.y, ymin, ymax);
+
+        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
+    }
+}
