9e3b46ef0d223bf5cbfe906e741f392b6b5db0c8
sarahc123 <g00330821@gmit.ie>
2018-11-29 17:26:01 +0000

    added fb sdk, to push high score to fb newsfeed

---
+++ Side Scrolling Platformer/Assets/_Scripts/FacebookManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Facebook.Unity;
+
+public class FacebookManager : MonoBehaviour {
+
+    public Text UserId;
+
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init();
+        }
+        else
+        {
+            FB.ActivateApp();
+        }
+    }
+
+    // function called when click on login button, running fb login with read permissions and then calling OnLogIn
+    public void LogIn()
+    {
+        FB.LogInWithReadPermissions(callback: OnLogIn);
+    }
+	
+    private void OnLogIn(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            AccessToken token = AccessToken.CurrentAccessToken;
+            UserId.text = token.UserId;
+        }
+        else
+        {
+            Debug.Log("cancelled  LogIn");
+        }
+    }
+}
---
+++ Side Scrolling Platformer/Assets/_Scripts/SplashController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour {
+
+    void Start()
+    {
+        StartCoroutine(LoadMainMenu());
+    }
+
+    IEnumerator LoadMainMenu()
+    {
+        Debug.Log("enter splashscreenController");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(0);// Load main menu after 2 seconds
+        Debug.Log("loaded scene 0");
+
+    }
+}
